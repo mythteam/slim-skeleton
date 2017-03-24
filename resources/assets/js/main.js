6b3952fcb16d7237a3c3cb1966fd3bea3cbb5814
lichunqiang <light-li@hotmail.com>
2017-03-24 18:20:54 +0800

    [chore] just split configurations

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -3,18 +3,15 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import Bus from './plugins/bus.js'
-import Config from './plugins/config'
+import ConfigPlugin from './plugins/config'
+import Config from './config'
 import router from './router'
-
-let _config = {
-  name: 'slim-application'
-}
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 Vue.use(Bus)
-Vue.use(Config, _config)
+Vue.use(ConfigPlugin, Config)
 
 /* eslint-disable no-new */
 new Vue({
